c8e941f0d068c4579b73941c5a7efde291f09563
LucaFumagalli99 <lucafu99@gmail.com>
2020-03-22 19:08:57 +0100

    Aggiunta variabili, metodi, costruttore eventi C I

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EsPerMe';
+  message:string = "Hello World"
+  message2:string = "ABC"
+  A:string // undefined
+
+  nome:string="Luca"
+  età:number=20
+  condizione:boolean
+  oggetto:{nome:string,età:number}
+  array=[]
+  arrayOggetti:[
+    {nome:string,età:number},
+    {nome:string,età:number}]
+  variabilequalsiasi//se assegnamo una stringa, un numero o altro diventerà automaticamente di quel tipo
+  variabilequalsiasi2: any// altro modo per indicarla
+  stampa:string
+
+  constructor()//è la funzione che al caricamento del component parte per prima dopo la dichiarazione delle variabili con this mi riferisco alle variabili esterne, in questo modo ho assegnato unvalore ad A.
+  {
+    this.A="A" 
+  }
+  
+  getClick()//funzione
+  {console.log("Ciao")
+    window.alert("Ciao")}
+
+    getClick2(pippo)//funzione
+    {console.log(pippo)}
+
+    getText(pippo)//funzione
+    {console.log(pippo.target.value)
+     this.stampa=pippo.target.value}
+
 }
-var message:string = "Hello World" 
